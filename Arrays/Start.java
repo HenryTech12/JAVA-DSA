4f907843c5146b1a02722b09eb770dadb946998a
harshjoshi <harsh@example.com>
2024-05-26 09:31:49 +0530

    add array

---
+++ Arrays/Start.java
@@ -0,0 +1,12 @@
+public class Start {
+    
+    // start with the create a array
+    // there are many ways to ctate a java array
+
+    int marks [] = new int [50];
+    // in this it stored the null values50
+
+    String fruits [] = {"apple", "banana", "mango"};
+    // in this it stored only the value which already given and make size of it
+    
+}
