2c554d297c2c622e57b5709ad80fed0507df032f
harshjoshi1312 <joshiharsh1312@gmail.com>
2025-03-16 18:23:17 +0530

    :chart_with_upwards_trend: Almighty Push

--- rename.py
+++ rename.py
@@ -7,10 +7,10 @@ def rename_folders(base_path):
     for folder in os.listdir(base_path):
         folder_path = os.path.join(base_path, folder)
         if os.path.isdir(folder_path):
-            # Check if folder name starts with digits (e.g., 1 - , 2 -)
+            # Check if folder name starts with digits (e.g., 001 - , 002 -)
             if folder.split(' - ')[0].isdigit():
                 folder_number = int(folder.split(' - ')[0])
-                new_name = f"{folder_number:03d} - {folder.split(' - ')[1]}"
+                new_name = f"{folder_number:04d} - {folder.split(' - ')[1]}"
             # If the folder name contains 'practice', remove it
             elif 'practice' in folder:
                 new_name = folder.replace('practice - ', '').strip()
