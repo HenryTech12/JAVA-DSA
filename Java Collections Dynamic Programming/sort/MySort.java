73e657a64a0af4c956226d712531c658e095bd4c
HenryTech12 <henrycrownny@gmail.com>
2025-04-27 08:02:29 +0100

    dynamic programming with java

---
+++ Java Collections Dynamic Programming/sort/MySort.java
@@ -0,0 +1,46 @@
+package sort;
+
+public class MySort
+{
+	private SortType type = SortType.ASC;
+	
+	
+	public int[] bubbleSort(int[] data, SortType mytype) {
+		for(int i = 0; i < data.length; i++) {
+			for(int j = 0; j < data.length - i - 1; j++) {
+				if(data[j] > data[j+1]) {
+					if(type.toString().equals(mytype.toString()))
+					  swap(data, j, j+1);
+			   }
+			   else {
+				  if(!type.toString().equals(mytype.toString()))
+				    swap(data,j,j+1);
+			   }
+			}
+		}
+		
+		return data;
+	}
+	
+	public void swap(int[] data, int index1, int index2) {
+		int temp = data[index1];
+		data[index1] = data[index2];
+		data[index2] = temp;
+	}
+	
+	public int[] selectionSort(int[] data) {
+	  for(int i = 0; i < data.length; i++) {
+		  int minIndex = i;
+			for(int j = i+1; j < data.length; j++) {
+				if(data[j] < data[minIndex]) {
+				    minIndex = j;
+				 }
+			} 
+			int temp = data[i];
+	        data[i] = data[minIndex];
+		    data[minIndex] = temp;
+		}
+		return data;
+	}
+
+}
